3ae84a3934732592d7965795d8d9648a4f98616b
ultravedant <122342177+ultravedant@users.noreply.github.com>
2026-04-26 22:57:35 +0530

    fixed issues 2

--- auth.config.ts
+++ auth.config.ts
@@ -37,8 +37,5 @@ export const authConfig = {
     },
   },
   providers: [],
-  trustHost: [
-    "https://bug-tracker-1-4p8r.onrender.com",
-    "https://localhost:10000",
-  ],
+  trustHost: true,
 } satisfies NextAuthConfig;
